81b64ed76eba7721ea520f3cbc01e4fbcd4af59c
SofianeAz <87657218+SofianeAz@users.noreply.github.com>
2022-02-13 17:41:57 +0100

    Fix  QUOTES

--- JavaScript/YodaQuote.js
+++ JavaScript/YodaQuote.js
@@ -1,7 +1,9 @@
 
 
 
-const yodaURL = 'https://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote';
+// const yodaURL = 'http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote';
+
+const yodaURL = 'javascript/quote.json';
 
 
 
@@ -11,18 +13,27 @@ async function getQuote(){
         console.log(error);
       });;
     console.log('dataYoda');
+    // console.log(data);
     createQuote(data);
 };
 getQuote();
 
+
+
+// console.log(getRandomInt(57));
 const yodaQuoteContainer = document.getElementById('yoda');
 
 async function createQuote(param){
-    yodaQuoteContainer.innerHTML = '';
-    const quote = document.createElement('h3');
-    quote.setAttribute('class', 'yodaquote');
-    quote.innerText += param.content;  
-    yodaQuoteContainer.appendChild(quote); 
+    let randomNum = getRandomInt(56);
+    for(i = 0; i < param.length; i++){
+        if(i === randomNum) {
+            yodaQuoteContainer.innerHTML = '';
+            const quote = document.createElement('h3');
+            quote.setAttribute('class', 'yodaquote');
+            quote.innerText += param[i].content;  
+            yodaQuoteContainer.appendChild(quote);
+        }
+    }
 
     // const imgQuote = document.createElement('img');
     // imgQuote.setAttribute('src', 'https://static.hitek.fr/img/actualite/ill_m/491826397/yodavspalpatine.jpg');
@@ -33,3 +44,8 @@ async function createQuote(param){
 const myNavigator = document.querySelector('nav');
 myNavigator.addEventListener('click', ()=> {
     getQuote(); });
+    
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)+1;
+}
